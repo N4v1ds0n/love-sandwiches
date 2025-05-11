5b2b2b52c52a513f35f8000abff490a29ce956fb
N4v1ds0n <damian.droste@gmx.de>
2025-05-11 20:31:51 +0200

    Add function to get last five entries in sheet

--- run.py
+++ run.py
@@ -85,6 +85,18 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Get the last 5 entries of sales data from the sales worksheet.
+    """
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """
     Run all program functions.
@@ -102,4 +114,7 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+#main()
+
+
+sales_columns = get_last_5_entries_sales()
